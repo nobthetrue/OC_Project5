947261408fecec48ff1b0223fef8315328c89570
Gianni Cavanna <giannicavanna74@gmail.com>
2024-11-14 11:24:22 +0100

    Ajout de React Router et ses dépendances + suppression fichiers inutiles

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+function Home() {
+     return (
+          <div className="Home">
+               <header className="Home-header">
+                    <h1>Coucou</h1>
+               </header>
+          </div>
+     )
+}
+
+export default Home
